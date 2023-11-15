faaa4f02f4ef066c7610b25ea019f36301b7d333
jonasvictor <jonas.victor@dcx.ufpb.br>
2023-11-15 14:28:18 -0300

    closing the basics.

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -228,7 +228,7 @@ describe("Central de Atendimento ao Cliente TAT", function () {
     });
   });
 
-  it.only("encontra o gato escondido", function () {
+  it("encontra o gato escondido", function () {
     cy.get("#cat").invoke("show").should("be.visible");
     cy.get("#title").invoke("text", "CAT TAT");
     cy.get("#subtitle").invoke("text", "Eu ❤️ gatos");
